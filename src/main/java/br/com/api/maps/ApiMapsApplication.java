f9b314f1ee228e91f35b5603be8131d97d9fb85e
PaulimJr <paulim.jr@hotmail.com>
2018-02-15 20:13:19 -0200

    primeiro commit

---
+++ src/main/java/br/com/api/maps/ApiMapsApplication.java
@@ -0,0 +1,12 @@
+package br.com.api.maps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMapsApplication.class, args);
+	}
+}
