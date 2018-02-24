a3b96169cf8f06a0e07124c1beac4789dc66132e
PaulimJr <paulim.jr@hotmail.com>
2018-02-24 16:53:51 -0300

    Config database and creating Services and Repositories

---
+++ src/main/java/br/com/api/maps/repository/CompanyRepository.java
@@ -0,0 +1,18 @@
+package br.com.api.maps.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.maps.entity.Company;
+
+/**
+ * Repository of {@link Company}
+ * 
+ * @author paulo
+ *
+ */
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Integer>{
+
+
+}
